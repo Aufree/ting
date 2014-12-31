14bc0990744ed0ee2bb9026f261ab57a8cccd04e
Aufree <freedomlijinfa@gmail.com>
2014-12-31 15:11:02 +0800

    Better error messages

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,18 +1,17 @@
 module SongsHelper
-  require 'nokogiri'
   require 'open-uri'
+  require 'json'
   
   def get_xiami_info(object = {})
-      url = "http://www.xiami.com/song/playlist/id/" + object.s_id.to_s
-
-      # Fetch xiami info
-      doc = Nokogiri::XML(open(url, 'User-Agent' => 'ruby'))
-      object.title = doc.search("title").text.to_s
-      object.artist = doc.search("playlist trackList artist").text.to_s
-      object.pic = doc.search("playlist trackList album_pic").text.to_s
-      if object.artist.empty?
+      begin
+        url = "http://inmusic.sinaapp.com/xiami_api/" + object.s_id.to_s
+        doc = JSON.parse(open(url).read)
+      rescue JSON::ParserError
         object.title = ""
-        errors[:base] << "#{I18n.t('songs.cant_fetch')}"
+      else
+        object.title = doc['title']
+        object.artist = doc['singer']
+        object.pic = doc['pic2x']
       end
   end
 end
